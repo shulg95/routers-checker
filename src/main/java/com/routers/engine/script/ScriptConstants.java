ee9ea8226f7855ea71a2ae55af7c9d3872e815aa
shulg95 <shulg95@mail.ru>
2017-05-19 20:57:28 +0300

    [master-groovy] Groovy scripts support added

---
+++ src/main/java/com/routers/engine/script/ScriptConstants.java
@@ -0,0 +1,9 @@
+package com.routers.engine.script;
+
+public final class ScriptConstants {
+
+    public static final String DEFAULT_SCRIPT_METHOD = "testMethod";
+
+    private ScriptConstants() {}
+
+}
